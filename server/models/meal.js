633838932b8317a59bbfbb72880e3820c4578b06
a5admian <114887569+a5admian@users.noreply.github.com>
2022-12-02 16:47:36 -0500

    Update meal.js

--- server/models/meal.js
+++ server/models/meal.js
@@ -3,8 +3,11 @@ let mongoose = require('mongoose');
 let mealModel = mongoose.Schema(
     {
         breakf: String,
+        c1: String,
         lunch: String,
+        c2: String,
         dinner: String,
+        c3: String,
     },
     {
         collection: "meal"
