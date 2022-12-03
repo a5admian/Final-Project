06771189c46ebc5e97ce20ed861815b784bb6fe6
a5admian <114887569+a5admian@users.noreply.github.com>
2022-12-03 18:54:52 -0500

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,10 @@ let indexController = require('../controller/index')
 
 router.get('/', indexController.displayHomePage);
 router.get('/home', indexController.displayHomePage);
-
+router.get('/login',indexController.displayLoginPage);
+router.post('/login',indexController.processLoginPage);
+router.get('/register',indexController.displayRegisterPage);
+router.post('/register',indexController.processRegisterPage);
+router.get('/logout',indexController.performLogout);
 
 module.exports = router;
