d36143ea0e5c201a289b8ef6a468e79f17771733
a5admian <114887569+a5admian@users.noreply.github.com>
2022-12-03 18:55:12 -0500

    Update meal.js

--- server/routes/meal.js
+++ server/routes/meal.js
@@ -1,23 +1,30 @@
 let express = require('express');
 let router = express.Router();
 let mongoose = require('mongoose');
-
 let Meal = require('../models/meal');
 let mealController = require('../controller/meal');
-const meal = require('../models/meal');
+
+function requireAuth(req,res,next)
+{
+    if(!req.isAuthenticated())
+    {
+        return res.redirect('/login');
+    }
+    next();
+}
 
 
 /*routes for different views and operations*/
 router.get('/',mealController.displayMealList);
 
-router.get('/add',mealController.displayAddPage);
+router.get('/add',requireAuth,mealController.displayAddPage);
 
-router.post('/add',mealController.processAddPage);
+router.post('/add',requireAuth,mealController.processAddPage);
 
-router.get('/edit/:id',mealController.displayEditPage);
+router.get('/edit/:id',requireAuth,mealController.displayEditPage);
 
-router.post('/edit/:id',mealController.processsEditPage);
+router.post('/edit/:id',requireAuth,mealController.processsEditPage);
 
-router.get('/delete/:id',mealController.performDeleteOperation);
+router.get('/delete/:id',requireAuth,mealController.performDeleteOperation);
 
 module.exports = router;
